dd8f60101b9d1491ea7b9bd8911df24b0b418052
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-01-25 08:47:03 -0800

    new file:   app/Http/Controllers/BarangController.php 	new file:   app/Http/Controllers/KasirController.php 	new file:   app/Http/Controllers/PelangganController.php 	new file:   app/Http/Controllers/Toko_SembakoController.php 	new file:   app/Http/Controllers/TransaksiController.php 	new file:   app/Models/kasir.php 	new file:   app/Models/pelanggan.php 	new file:   app/Models/transaksi.php 	deleted:    database/migrations/2025_01_24_173316_create_barangs_table.php 	new file:   database/migrations/2025_01_25_163031_create_barangs_table.php 	new file:   resources/views/barang/index.blade.php

--- database/migrations/2025_01_24_173316_create_barangs_table.php
+++
@@ -1,9 +0,0 @@
-public function up(): void
-{
-    Schema::create('posts', function (Blueprint $table) {
-        $table->id();
-        $table->string('image');
-        $table->string('title');
-        $table->text('content');
-        $table->timestamps();
-    });
---
+++ database/migrations/2025_01_25_163031_create_barangs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->string('kode_barang,6')->unique()->primary();
+            $table->string('nama_barang',30);
+            $table->bigInteger('harga');
+            $table->bigInteger('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
