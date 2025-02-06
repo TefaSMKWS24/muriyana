6ed2a35fc73a476a8f39b49e77e964f04cd8ea99
Tefaproject <tefaproject.smk@gamil.com>
2025-02-06 18:48:31 +0700

    modified:   app/Http/Middleware/Authenticate.php 	modified:   app/Http/Middleware/RedirectIfAuthenticated.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,14 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if(!$request->expectsJson()){
+
+            if ($request->is('admin/*')) {
+                return route('login.admin');
+            }
+            if ($request->is('kasir/*')) {
+                return route('login.kasir');
+            }
+        }
     }
 }
