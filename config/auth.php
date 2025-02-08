454b5df3e6c8f86a62267d5ccea56b1acaaf8d71
tefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-08 10:36:14 -0800

    modified:   config/auth.php

--- config/auth.php
+++ config/auth.php
@@ -40,7 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-    ],
+
 
         'kasir' => [
             'driver' => 'session',
@@ -51,7 +51,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-    ];
+    ],
 
     /*
     |--------------------------------------------------------------------------
