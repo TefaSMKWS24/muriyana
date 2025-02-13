30d446f1cb2a788ebb9d88e69d51a07ce5a7cdd9
tefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-13 09:24:37 -0800

    modified:   app/Http/Controllers/AuthController.php 	modified:   app/Models/data_kasir.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,56 +5,54 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-
 class AuthController extends Controller
 {
-
-    public function loginkasir(request $request)
+    public function loginkasir(Request $request)
     {
-if(auth::guard('kasir')->attempt([
-    'nik' => $request->nis,
-    'password' => $request->password]))
-    {
-        dd('berhasil: '.auth::guard('kasir')->user());
-        log::info('login succeessful');
-        //return redirect('/user/dashboarrd');
-    }
-    else{
-        echo "login gagal";
-        //return redirect('/user')->with('warninng', 'NIS / password salah');
-    }
-}
 
+
+
+        if(Auth::guard('kasir')->attempt([
+                                    'kode_kasir' => $request->kode_kasir,
+                                    'password' => $request->password]))
+        {
+            dd('Berhasil: '.Auth::guard('kasir')->user());
+            Log::info('Login successful');
+        //return redirect('/kasir/dashboard');
+        }
+        else{
+         echo "Login Gagal";
+            return redirect('/kasir')->with('warning', 'NIS / Password Salah!');
+        }
+    }
     public function logoutkasir()
     {
-        if(auth::guard('kasir')->check()){
-            auth::guard('kasir')->logout();
+        if(Auth::guard('kasir')->check()){
+            Auth::guard('kasir')->logout();
             return redirect('/');
         }
     }
-    public function loginadmin(request $request)
+
+    public function loginadmin(Request $request)
     {
         if(Auth::guard('admin')->attempt([
-
-
-            'email' => $request->email,
-            'password' => $request->password]))
-            {
-                dd('berhasil: '.auth::guard('admin')->user());
-                log::info('login successful');
-                //return redirect('/user/dashboard');
-            }
-            else{
-                echo "login gagal";
-                //return redirect('/user')->with('warninng', 'NIS / password salah');
-            }
+                                    'email' => $request->email,
+                                    'password' => $request->password]))
+        {
+            dd('Berhasil: '.Auth::guard('admin')->user());
+            Log::info('Login successful');
+            //return redirect('/admin/dashboard');
+        }
+        else{
+           echo "Login Gagal";
+            return redirect('/admin')->with('warning', 'NIS / Password Salah!');
+        }
     }
     public function logoutadmin()
     {
-        if(auth::guard('admin')->check()){
-            auth::guard('admin')->logout();
+        if(Auth::guard('admin')->check()){
+            Auth::guard('admin')->logout();
             return redirect('/');
         }
     }
-
 }
--- app/Models/data_kasir.php
+++ app/Models/data_kasir.php
@@ -2,13 +2,46 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class data_kasir extends Model
+class data_kasir extends Authenticatable
 {
-    use HasFactory;
-    
+    use HasApiTokens, HasFactory, Notifiable;
+
     protected $table = 'data_kasir';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'kode_kasir',
+        'password',
+        'nama_kasir',
+        'nohp',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
