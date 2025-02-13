7229a54af2378baeff5f2f2ef54fba340ef9c27d
tefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-13 11:29:07 -0800

    renamed:    database/seeders/pelangganseeder/barangseeder -> database/seeders/barangseeder.php 	new file:   database/seeders/kategoriseeder.php 	new file:   database/seeders/pelangganseeder.php 	new file:   database/seeders/supplierseeder.php 	new file:   database/seeders/transaksiseeder.php

--- database/seeders/barangseeder.php
+++ database/seeders/barangseeder.php
@@ -10,30 +10,24 @@ class barangseeder extends Seeder
     /**
      * Run the database seeds.
      */
-public function run(): void
+    public function run(): void
     {
         $data = [
             [
                 'kode_barang' => 'B001',
-                'nama_barang' => 'minyak',
-                'harga' => 10000,
-                'stok' => 10,
+                'nama_barang' => 'Beras',
+                'harga' => '5000',
+                'stok' => '100',
                 'kode_kategori' => 'K001',
             ],
             [
                 'kode_barang' => 'B002',
-                'nama_barang' => 'garam',
-                'harga' => 5000,
-                'stok' => 5,
+                'nama_barang' => 'Gula',
+                'harga' => '5000',
+                'stok' => '100',
                 'kode_kategori' => 'K002',
-
-            ]
+            ],
         ];
         DB::table('barang')->insert($data);
     }
 }
-
-
-
-
-
